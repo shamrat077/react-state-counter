574bbde7b081489a919ee9a72e6cd73f16d39aa6
shamrat077 <shamrat077@gmail.com>
2022-04-11 00:35:52 +0600

    complete state counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increaseHandler = () => {
+    setCounter(counter + 1);
+  };
+
+  const decreaseHandler = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increaseHandler} className="increase__btn">
+        Increased Value By 1
+      </button>
+      <button onClick={decreaseHandler} className="decrease__btn">
+        Decreased Value By 1
+      </button>
+      <span className="value">value: {counter}</span>
     </div>
   );
 }
